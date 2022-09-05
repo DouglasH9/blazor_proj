0486b7288532b94838c5a89c8f16b29c4d62b49b
DouglasH9 <douglash9882@gmail.com>
2022-09-04 20:34:38 -0500

    two way binding

---
+++ blazor_proj_class_files/LearnBlazorModels/Demo_Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace blazor_proj_class_files.LearnBlazorModels
+{
+    public class Demo_Product
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public double Price { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
+
