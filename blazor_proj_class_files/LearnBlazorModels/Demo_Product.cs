2cb29e444417b8d4e2ded2617d43e1795a1ddcb5
DouglasH9 <douglash9882@gmail.com>
2022-09-06 16:34:55 -0500

    added new functionality

--- blazor_proj_class_files/LearnBlazorModels/Demo_Product.cs
+++ blazor_proj_class_files/LearnBlazorModels/Demo_Product.cs
@@ -10,6 +10,8 @@ namespace blazor_proj_class_files.LearnBlazorModels
         public double Price { get; set; }
 
         public bool IsActive { get; set; }
+
+        public List<Demo_ProductProp>? ProductProperties { get; set; }
     }
 }
 
---
+++ blazor_proj_class_files/LearnBlazorModels/Demo_ProductProp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace blazor_proj_class_files.LearnBlazorModels
+{
+    public class Demo_ProductProp
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+        public Demo_ProductProp()
+        {
+        }
+    }
+}
+
