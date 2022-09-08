689a58bd1b18950ce5632df17fb552f843e5323b
DouglasH9 <douglash9882@gmail.com>
2022-09-07 23:19:59 -0500

    toastr js

---
+++ blazor_proj_server/wwwroot/JS/Common.js
@@ -0,0 +1,9 @@
+﻿window.showToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Great success!", { timeOut: 5000 });
+    }
+     
+    if (type === "error") {
+        toastr.error(message, "Uh ohs!", { timeOut: 5000 });
+    }
+}
